52e82cfa970d99650b290aace451a982c05381fb
Ali Kızıldağ <ali.kizildag@inksen.com>
2021-05-13 18:01:19 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Course
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] //Id olduğunu mongoDb'ye bu şekilde aktarıyoruz.
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public string Picture { get; set; }
+
+        [BsonRepresentation(BsonType.DateTime)]
+        public DateTime CreatedTime { get; set; }
+
+        public Feature Feature { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryId { get; set; }
+
+        [BsonIgnore] //MongoDb oluşurken bu property'i veritabanına yansıtma.
+        public Category Category { get; set; }
+
+    }
+}
---
+++ Shared/FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeCourse.Shared.ControllerBases
+{
+   public class CustomBaseController:ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response) //response'den 400'de gelirse 400 göndercek.200 gelirse 200 göndercek.
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
